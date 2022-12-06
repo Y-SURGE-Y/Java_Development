dd5922f84e7df1feba1e2486110ccefd9baa3f75
Anand Krishn Mishra <92173758+Y-SURGE-Y@users.noreply.github.com>
2022-12-06 22:19:59 +0530

    Palindrome Number (Three Digit)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        boolean b = arnums(a);
+        System.out.println(b);
+
+    }
+    static boolean arnums(int a){
+        int sum=0;
+        int temp = a;
+        while (a>0) {
+            int rem = 0;
+            rem = a % 10;
+            int c = rem * rem * rem;
+            sum+=c;
+            a = a / 10;
+        }
+        if (sum == temp){
+            return true;
+        }
+        else
+            return false;
     }
 }
