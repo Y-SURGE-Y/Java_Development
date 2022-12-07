565344094df05f42ec520bbb5e6c9e7e8bfc054c
Anand Krishn Mishra <92173758+Y-SURGE-Y@users.noreply.github.com>
2022-12-07 08:57:01 +0530

    Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = fact(a);
+        boolean b = palin(a);
         System.out.println(b);
     }
-        static int fact(int a){
-            int sum = 1;
-            for(int i = a ; i>1 ; i--){
-                sum*=i;
+        static boolean palin(int a){
+            String str = String.valueOf(a);
+            boolean res = true;
+            int n = str.length();
+            for (int i = 0 ; i< n/2 ; i++ ){
+                if (str.charAt(i) == str.charAt(n-i-1)){
+                     res = true;
+                }else{
+                    res = false;
+                    break;
+                }
             }
-            return sum;
+        return res;
         }
     }
