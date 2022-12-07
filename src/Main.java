4f4568943fbb2702d81b0727933d2a977cf32fa2
Anand Krishn Mishra <92173758+Y-SURGE-Y@users.noreply.github.com>
2022-12-07 19:14:12 +0530

    125. Valid Palindrome (Leetcode)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,36 @@
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        boolean b = palin(a);
-        System.out.println(b);
-    }
-        static boolean palin(int a){
-            String str = String.valueOf(a);
-            boolean res = true;
-            int n = str.length();
-            for (int i = 0 ; i< n/2 ; i++ ){
-                if (str.charAt(i) == str.charAt(n-i-1)){
-                     res = true;
-                }else{
-                    res = false;
-                    break;
+        public static void main (String args[]) {
+            Scanner sc =new Scanner(System.in);
+            String s = sc.nextLine();
+            if(s == null){
+                System.out.println("Is Palindrome");;
+            }
+            int n = s.length();
+            int b = 0;
+            StringBuilder str = new StringBuilder();
+            int k = 0;
+            for(int i = 0 ; i<n ; i++){
+                if(Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i)) ){
+                    str.append(s.charAt(i));
                 }
             }
-        return res;
+            int a = str.length();
+            while(k<a){
+                str.setCharAt(k,Character.toLowerCase(str.charAt(k)));
+                k++;
+            }
+            if(a==1){
+                System.out.println("Is Palindrome");;
+            }
+            for(int i = 0 ; i<a/2 ; i++){
+                if(str.charAt(i) == str.charAt(a-i-1)){
+                    b++;
+                }
+            }
+            if(b == a/2){
+                System.out.println("Is Palindrome");
+            }else
+                System.out.println("Not Palindrome");
         }
-    }
+}
